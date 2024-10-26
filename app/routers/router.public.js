9791f624f59fb70a43a05140a6cd057cf6d73743
PeterLeSouchu <peter22510@gmail.com>
2024-10-26 18:54:47 +0200

    add otp for signup + redis + jwt

--- app/routers/router.public.js
+++ app/routers/router.public.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import authController from '../controllers/public-controllers/auth-controller.js';
 const publicRouter = Router();
 
-publicRouter.post('/api/signup', authController.sendOTP);
-publicRouter.get('/api/test', authController.test);
+publicRouter.post('/api/signup/otp', authController.sendOTP);
+publicRouter.post('/api/signup/register', authController.registerUser);
 
 export default publicRouter;
