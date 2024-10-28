4be504b43e441f119e91d85958af13e443a531ba
PeterLeSouchu <peter22510@gmail.com>
2024-10-28 12:42:17 +0100

    feat: change password finish

--- app/routers/router.public.js
+++ app/routers/router.public.js
@@ -1,10 +1,19 @@
 import { Router } from 'express';
 import signupController from '../controllers/public-controllers/signup-controller.js';
 import signinController from '../controllers/public-controllers/signin-controller.js';
+import forgotPasswordController from '../controllers/public-controllers/forgot-password-controller.js';
 const publicRouter = Router();
 
 publicRouter.post('/api/signup/otp', signupController.sendOTP);
 publicRouter.post('/api/signup/register', signupController.registerUser);
 publicRouter.post('/api/signin', signinController.login);
+publicRouter.post(
+  '/api/forgot-password',
+  forgotPasswordController.sendResetLink
+);
+publicRouter.patch(
+  '/api/reset-password',
+  forgotPasswordController.resetPassword
+);
 
 export default publicRouter;
