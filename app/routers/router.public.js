1f8c865f32609d07f1dcae9b531fa0b2d99e3eea
PeterLeSouchu <peter22510@gmail.com>
2024-10-29 16:47:31 +0100

    fix; facotorize router with trycatchmiddleware

--- app/routers/router.public.js
+++ app/routers/router.public.js
@@ -2,18 +2,18 @@ import { Router } from 'express';
 import signupController from '../controllers/public-controllers/signup-controller.js';
 import signinController from '../controllers/public-controllers/signin-controller.js';
 import forgotPasswordController from '../controllers/public-controllers/forgot-password-controller.js';
+import tryCatchMiddleware from '../utils/errors/try-catch-middleware.js';
+
 const publicRouter = Router();
 
-publicRouter.post('/api/signup/otp', signupController.sendOTP);
-publicRouter.post('/api/signup/register', signupController.registerUser);
-publicRouter.post('/api/signin', signinController.login);
-publicRouter.post(
-  '/api/forgot-password',
-  forgotPasswordController.sendResetLink
-);
-publicRouter.patch(
-  '/api/reset-password',
-  forgotPasswordController.resetPassword
-);
+const route = (method, path, controller) => {
+  publicRouter[method](path, tryCatchMiddleware(controller));
+};
+
+route('post', '/api/signup/otp', signupController.sendOTP);
+route('post', '/api/signup/register', signupController.registerUser);
+route('post', '/api/signin', signinController.login);
+route('post', '/api/forgot-password', forgotPasswordController.sendResetLink);
+route('patch', '/api/reset-password', forgotPasswordController.resetPassword);
 
 export default publicRouter;
