1f1046b4f59186782b2f016ea335aa0ac6741cd6
PeterLeSouchu <peter22510@gmail.com>
2024-10-31 12:38:25 +0100

    feat: add new schema

--- app/validation/schemas/form-schema/singup-schema.js
+++ app/validation/schemas/form-schema/singup-schema.js
@@ -1,7 +1,6 @@
 import Joi from 'joi';
-import { emailSchema, passwordSchema, pseudoSchema } from './inputs-schema';
+import { emailSchema, passwordSchema, pseudoSchema } from '../inputs-schema';
 
-// Création du schéma d'inscription
 export const signupSchema = Joi.object({
   email: emailSchema,
   pseudo: pseudoSchema,
