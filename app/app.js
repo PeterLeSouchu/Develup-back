09f86cea09c0c7e7e2717af786088e646d4509de
PeterLeSouchu <peter22510@gmail.com>
2024-11-24 11:32:34 +0100

    websocket

--- app/app.js
+++ app/app.js
@@ -6,6 +6,8 @@ import cookieParser from 'cookie-parser';
 import { errorHandler } from './errors/error-handler-middleware.js';
 import http from 'http';
 import { Server } from 'socket.io';
+import jwt from 'jsonwebtoken';
+import conversationDatamapper from './datamappers/conversation-datamapper.js';
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -28,14 +30,114 @@ app.use(errorHandler);
 
 const server = http.createServer(app);
 
-export const io = new Server(server, {
+const io = new Server(server, {
   cors: {
     origin: 'http://localhost:5173',
     // methods: ['GET', 'POST'],
     credentials: true,
   },
+  cookie: true,
 });
 
-app.listen(process.env.PORT, () => {
+io.use((socket, next) => {
+  try {
+    const token = socket.handshake.headers.cookie
+      ? socket.handshake.headers.cookie
+          .split(';')
+          .find((cookie) => cookie.trim().startsWith('jwt='))
+          .split('=')[1]
+      : null;
+
+    if (!token) {
+      return socket.emit('error', 'Erreur innatendue, réessayez plus tard');
+    }
+
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    socket.user = user;
+    next();
+  } catch (err) {
+    return socket.emit('error', 'Erreur innatendue, réessayez plus tard');
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('client connected');
+  socket.on('joinConversation', async (conversationId) => {
+    try {
+      const isAllowed =
+        await conversationDatamapper.checkIfUserIsInConversation(
+          socket.user.id,
+          conversationId
+        );
+
+      if (!isAllowed) {
+        return socket.emit('error', 'Accès interdit à cette conversation');
+      }
+
+      socket.join(conversationId);
+    } catch (error) {
+      console.error(
+        'Erreur lors de la tentative de rejoindre une conversation :',
+        error
+      );
+      socket.emit(
+        'error',
+        'Erreur serveur lors de la tentative de rejoindre la conversation'
+      );
+    }
+  });
+
+  socket.on('newMessage', async (messageData) => {
+    try {
+      const { message, conversationId } = messageData;
+
+      const isAllowed =
+        await conversationDatamapper.checkIfUserIsInConversation(
+          socket.user.id,
+          conversationId
+        );
+
+      if (!isAllowed) {
+        return socket.emit('error', 'Action interdite');
+      }
+
+      const savedMessage = await conversationDatamapper.saveMessage(
+        message,
+        socket.user.id,
+        conversationId
+      );
+      const timestamp = savedMessage.created_at;
+      const date = new Date(timestamp);
+
+      const day = date.getDate().toString().padStart(2, '0');
+      const month = (date.getMonth() + 1).toString().padStart(2, '0');
+      const year = date.getFullYear();
+      const hours = date.getHours().toString().padStart(2, '0');
+      const minutes = date.getMinutes().toString().padStart(2, '0');
+
+      const formattedDate = `le ${day}/${month}/${year} à ${hours}h${minutes}`;
+
+      const messageToSendToClient = {
+        id: savedMessage.id,
+        date: formattedDate,
+        content: savedMessage.content,
+        isMe: savedMessage.user_id === socket.user.id ? true : false,
+      };
+
+      console.log('Message envoyé :', messageToSendToClient);
+
+      io.to(conversationId).emit('newMessage', messageToSendToClient);
+    } catch (error) {
+      console.error("Erreur lors de l'envoi d'un message :", error);
+      socket.emit('error', "Erreur serveur lors de l'envoi du message");
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`Utilisateur déconnecté : ${socket.user.id}`);
+  });
+});
+
+server.listen(process.env.PORT, () => {
   console.log(`App listening on ${process.env.HOST}`);
 });
--- app/websocket-config.js
+++ app/websocket-config.js
@@ -1,26 +1,23 @@
 import { io } from './app';
-import ApiError from './errors/error';
 import conversationDatamapper from './datamappers/conversation-datamapper';
 import jwt from 'jsonwebtoken';
 
 // Middleware to verify jwt
 io.use((socket, next) => {
   const token = socket.handshake.headers.cookie?.jwt;
+  console.log(token);
 
   if (!token) {
-    return next(
-      new ApiError('Erreur inattendue, essayer de vous  reconnecter', 401)
-    );
+    return socket.emit('error', 'Erreur innatendue, réessayez plus tard');
   }
 
   try {
     const user = jwt.verify(token, process.env.JWT_SECRET);
     socket.user = user;
+    console.log('is okssss');
     next();
   } catch (err) {
-    return next(
-      new ApiError('Erreur inattendue, essayer de vous  reconnecter', 401)
-    );
+    return socket.emit('error', 'Erreur innatendue, réessayez plus tard');
   }
 });
 
@@ -28,36 +25,67 @@ io.on('connection', (socket) => {
   console.log(`Utilisateur connecté : ${socket.user.id}`);
 
   socket.on('joinConversation', async (conversationId) => {
-    console.log(`${socket.user.id} rejoint la conversation ${conversationId}`);
+    try {
+      console.log(
+        `${socket.user.id} rejoint la conversation ${conversationId}`
+      );
 
-    const isAllowed = await conversationDatamapper.checkIfUserIsInConversation(
-      socket.user.id,
-      conversationId
-    );
-    if (!isAllowed) {
-      return socket.emit('error', 'Accès interdit à cette conversation');
+      const isAllowed =
+        await conversationDatamapper.checkIfUserIsInConversation(
+          socket.user.id,
+          conversationId
+        );
+
+      if (!isAllowed) {
+        return socket.emit('error', 'Accès interdit à cette conversation');
+      }
+
+      socket.join(conversationId);
+    } catch (error) {
+      console.error(
+        'Erreur lors de la tentative de rejoindre une conversation :',
+        error
+      );
+      socket.emit(
+        'error',
+        'Erreur serveur lors de la tentative de rejoindre la conversation'
+      );
     }
-
-    socket.join(conversationId);
   });
 
   socket.on('newMessage', async (messageData) => {
-    const isAllowed = await conversationDatamapper.checkIfUserIsInConversation(
-      message.conversationId,
-      socket.user.id
-    );
-    if (!isAllowed) {
-      return socket.emit('error', 'Action interdite');
-    }
+    console.log('on est dans le on de newMessage');
+    try {
+      const { message, conversationId } = messageData;
 
-    const { message, conversationId } = messageData;
-    // Sauvegarder le message dans la base de données
-    const savedMessage = await conversationDatamapper.saveMessage(
-      message,
-      socket.user.id,
-      conversationId
-    );
-    io.to(conversationId).emit('newMessage', savedMessage);
+      const isAllowed =
+        await conversationDatamapper.checkIfUserIsInConversation(
+          conversationId,
+          socket.user.id
+        );
+
+      if (!isAllowed) {
+        return socket.emit('error', 'Action interdite');
+      }
+
+      const savedMessage = await conversationDatamapper.saveMessage(
+        message,
+        socket.user.id,
+        conversationId
+      );
+
+      const messageToSendToClient = {
+        id: savedMessage.id,
+        date: savedMessage.created_at,
+        content: savedMessage.content,
+        isMe: savedMessage.user_id === socket.user.id ? true : false,
+      };
+
+      io.to(conversationId).emit('newMessage', messageToSendToClient);
+    } catch (error) {
+      console.error("Erreur lors de l'envoi d'un message :", error);
+      socket.emit('error', "Erreur serveur lors de l'envoi du message");
+    }
   });
 
   socket.on('disconnect', () => {
