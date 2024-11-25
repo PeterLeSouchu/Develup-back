3aa426ed1058bcb29820fbaa6a512b527969c06f
PeterLeSouchu <peter22510@gmail.com>
2024-11-25 15:44:22 +0100

    add redis prod

--- app/database/redis.js
+++ app/database/redis.js
@@ -1,3 +1,11 @@
 import Redis from 'ioredis';
 
-export const redis = new Redis();
+// export const redis = new Redis(); // local
+
+export const redis = new Redis({
+  host: process.env.REDISHOST,
+  port: process.env.REDISPORT,
+  password: process.env.REDISPASSWORD,
+  username: process.env.REDISUSER,
+  tls: {}, // Activate SSL
+});
