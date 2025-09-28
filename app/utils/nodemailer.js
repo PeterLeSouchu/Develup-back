9f2b9336e84c26d3a17213d8062167c9f5a8b4dd
Peter Le Souchu <peter22510@gmail.com>
2025-09-28 12:17:21 +0200

    feat: add log to debug reset password

--- app/utils/nodemailer.js
+++ app/utils/nodemailer.js
@@ -4,7 +4,7 @@ export const transporter = nodemailer.createTransport({
   service: "gmail",
   host: "smtp.gmail.com",
   port: 465,
-  secure: false,
+  secure: true,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASSWORD,
