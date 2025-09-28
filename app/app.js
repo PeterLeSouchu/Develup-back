f8f2cda50bc764b8170c19ec9218517940247925
Peter Le Souchu <peter22510@gmail.com>
2025-09-28 20:01:59 +0200

    feat: disabled otp + reset password v2

--- app/app.js
+++ app/app.js
@@ -1,28 +1,28 @@
-import 'dotenv/config';
-import express from 'express';
-import router from './routers/router.js';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import { errorHandler } from './errors/error-handler-middleware.js';
-import http from 'http';
-import { Server } from 'socket.io';
-import jwt from 'jsonwebtoken';
-import conversationDatamapper from './datamappers/conversation-datamapper.js';
-import path from 'path';
+import "dotenv/config";
+import express from "express";
+import router from "./routers/router.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { errorHandler } from "./errors/error-handler-middleware.js";
+import http from "http";
+import { Server } from "socket.io";
+import jwt from "jsonwebtoken";
+import conversationDatamapper from "./datamappers/conversation-datamapper.js";
+import path from "path";
 
 const app = express();
 
 const __dirname = path.resolve();
 
 // This repo is in reality a mono repo when we use the front-end code build named "dist" in public folder, so we use express.static to serve it
-app.use(express.static(path.join(__dirname, 'public/dist')));
+app.use(express.static(path.join(__dirname, "public/dist")));
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
-    // origin: ['https://develup.up.railway.app'],
+    // origin: ['http://localhost:5173'],
+    origin: ["https://develup.up.railway.app"],
     credentials: true,
     withCredentials: true,
   })
@@ -52,27 +52,27 @@ io.use((socket, next) => {
   try {
     const token = socket.handshake.headers.cookie
       ? socket.handshake.headers.cookie
-          .split(';')
-          .find((cookie) => cookie.trim().startsWith('jwt='))
-          .split('=')[1]
+          .split(";")
+          .find((cookie) => cookie.trim().startsWith("jwt="))
+          .split("=")[1]
       : null;
 
     if (!token) {
-      return socket.emit('error', 'Erreur inattendue, réessayez plus tard');
+      return socket.emit("error", "Erreur inattendue, réessayez plus tard");
     }
 
     const user = jwt.verify(token, process.env.JWT_SECRET);
     socket.user = user;
     next();
   } catch (err) {
-    return socket.emit('error', 'Erreur inattendue, réessayez plus tard');
+    return socket.emit("error", "Erreur inattendue, réessayez plus tard");
   }
 });
 
 // Serveur Socket
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   console.log(`Utilisateur connecté : ${socket.user.id}`);
-  socket.on('joinConversation', async (conversationId) => {
+  socket.on("joinConversation", async (conversationId) => {
     try {
       const isAllowed =
         await conversationDatamapper.checkIfUserIsInConversation(
@@ -81,23 +81,23 @@ io.on('connection', (socket) => {
         );
 
       if (!isAllowed) {
-        return socket.emit('error', 'Accès interdit à cette conversation');
+        return socket.emit("error", "Accès interdit à cette conversation");
       }
 
       socket.join(conversationId);
     } catch (error) {
       console.error(
-        'Erreur lors de la tentative de rejoindre une conversation :',
+        "Erreur lors de la tentative de rejoindre une conversation :",
         error
       );
       socket.emit(
-        'error',
-        'Erreur serveur lors de la tentative de rejoindre la conversation'
+        "error",
+        "Erreur serveur lors de la tentative de rejoindre la conversation"
       );
     }
   });
 
-  socket.on('newMessage', async (messageData) => {
+  socket.on("newMessage", async (messageData) => {
     try {
       const { message, conversationId } = messageData;
 
@@ -108,7 +108,7 @@ io.on('connection', (socket) => {
         );
 
       if (!isAllowed) {
-        return socket.emit('error', 'Action interdite');
+        return socket.emit("error", "Action interdite");
       }
 
       const savedMessage = await conversationDatamapper.saveMessage(
@@ -119,11 +119,11 @@ io.on('connection', (socket) => {
       const timestamp = savedMessage.created_at;
       const date = new Date(timestamp);
 
-      const day = date.getDate().toString().padStart(2, '0');
-      const month = (date.getMonth() + 1).toString().padStart(2, '0');
+      const day = date.getDate().toString().padStart(2, "0");
+      const month = (date.getMonth() + 1).toString().padStart(2, "0");
       const year = date.getFullYear();
-      const hours = date.getHours().toString().padStart(2, '0');
-      const minutes = date.getMinutes().toString().padStart(2, '0');
+      const hours = date.getHours().toString().padStart(2, "0");
+      const minutes = date.getMinutes().toString().padStart(2, "0");
 
       const formattedDate = `le ${day}/${month}/${year} à ${hours}h${minutes}`;
 
@@ -134,14 +134,14 @@ io.on('connection', (socket) => {
         user_id: socket.user.id,
       };
 
-      io.to(conversationId).emit('newMessage', messageToSendToClient);
+      io.to(conversationId).emit("newMessage", messageToSendToClient);
     } catch (error) {
       console.error("Erreur lors de l'envoi d'un message :", error);
-      socket.emit('error', "Erreur serveur lors de l'envoi du message");
+      socket.emit("error", "Erreur serveur lors de l'envoi du message");
     }
   });
 
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     console.log(`Utilisateur déconnecté : ${socket.user.id}`);
   });
 });
